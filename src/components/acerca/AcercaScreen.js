365c3c559bae98f1e89c05a3bc498f63197e7c51
Oscar <oscarvil52@gmail.com>
2022-06-01 19:24:14 -0500

    010622Osc

--- src/components/acerca/AcercaScreen.js
+++ src/components/acerca/AcercaScreen.js
@@ -3,15 +3,48 @@ import '../acerca/acerca.css';
 export const AcercaScreen = () => {
   return (
     <div>
-        <h1 class="justify-content-center titulo">Tecnologico de morelia</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+      <h1 class="justify-content-center titulo  ">Tecnologico de morelia</h1>
+      <div class="row">
+      <div class = "col">
+        
+        <p class="justify_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
         </p>
-    </div>
+      </div> 
+      <div class = "col" >
+      <div class="slide image_center">
+        <img src="https://educart.mx/wp-content/uploads/2020/03/2308-1.jpg" class="d-block w-50"   />
+      </div>
+      </div>
+ 
+
+        </div>
+        <div class="row">
+
+      <div class = "col" >
+      <div class="slide image_center">
+        <img src="https://educart.mx/wp-content/uploads/2020/03/2308-1.jpg" class="d-block w-50"   />
+      </div>
+      </div>
+      <div class = "col">
+        {/* <h1 class="justify-content-center titulo ">Tecnologico de morelia</h1> */}
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut atque commodi corrupti, corporis incidunt debitis vel aperiam veritatis nostrum quo deserunt aliquam omnis. Sint quam temporibus placeat vitae voluptate ullam.
+        </p>
+      </div> 
+
+        </div>
+        </div>  
+        
   )
 }
